eee9c8051cc20dde18d7892fc3178daff84311f1
Connor Novak <connor.novak@students.olin.edu>
2023-03-05 20:35:05 -0800

    log-tools Module (#226)
    * Initialize log-tools module with poetry.
    
    * BROKEN: move path_planner.log into log-tools module
    
    * Enforce python<3.12 on path-planner module
    
    * Convert path-planner to using log-tools
    
    * Convert robot-demos to using log-tools
    
    * Convert robot to using log-tools
    
    * Convert can_bus to using log-tools
    
    * Add monkeypatch grpc objects to can_bus
    
    * WIP on log reprs
    
    * Add log directory and ignore internal logs
    
    * Fix grpc pb2 object __repr__ monkeypatch
    
    * make log file ignore actually work
    
    * Accidentally fucked up the log file, fixing
    
    * Move logging configuration to log-tools and make better
    
    * Update robot_demos demos to use log_tools.basic_logging_config
    
    * Remove unused imports from robot_demos.utils
    
    ---------
    
    Co-authored-by: cnovak@shim-0.1 <connor@trilo.bio>
    Co-authored-by: yellow-pi <yellow-pi@trilo.bio>
    Co-authored-by: red-pi <red-pi@trilo.bio>

---
+++ py_organelles/log_tools/__init__.py
@@ -0,0 +1,140 @@
+"""Miscellaneous helpful logging tools."""
+import functools
+import inspect
+import logging
+import pathlib
+import tempfile
+from typing import Callable, List, Optional
+
+
+def log_enter_exit_factory(logger: logging.Logger) -> Callable:
+    """Creates a decorator that logs to a provided logger."""
+
+    # Copied with modifications from aceta/can_bus/can_bus/client.py
+    def log_enter_exit(func: Callable) -> Callable:
+        """Decorator to log function arguments and return values for debugging purposes.
+        """
+
+        @functools.wraps(func)
+        def dec(*args, **kwargs):
+            func_args = inspect.signature(func).bind(*args, **kwargs).arguments
+            # Unpack function arguments into DEBUG log message
+            logger.debug(
+                "%s(%s)",
+                func.__qualname__,
+                ", ".join(
+                    map("{0[0]}={0[1]!r}".format, # pylint: disable=consider-using-f-string
+                    [(k, v) for k, v in func_args.items() if k != "self"])
+                )
+            )
+
+            try:
+                retval = func(*args, **kwargs)
+            except Exception as err:
+                logger.error(err)
+                logger.debug(err, exc_info=True)
+                raise
+
+            # log return values
+            retval_tuple = (retval,) if not isinstance(retval, tuple) else retval
+            logger.debug(
+                "%s(...) returned %s",
+                func.__qualname__,
+                ", ".join(repr(value) for value in retval_tuple))
+
+            return retval
+        return dec
+    return log_enter_exit
+
+
+COLOR_BRIGHT_RED = '\x1b[91m'
+COLOR_BRIGHT_YELLOW = '\x1b[93m'
+COLOR_RED = '\x1b[31m'
+COLOR_BLUE = '\x1b[34m'
+COLOR_WHITE = '\x1b[37m'
+COLOR_RESET = '\x1b[0m'
+
+
+class ColorFormatter(logging.Formatter):
+    """Formatter that uses ANSI escape codes to color messages based on logging level.
+
+    References:
+        https://alexandra-zaharia.github.io/posts/make-your-own-custom-color-formatter-with-python-logging/
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Instantiate new ColorFormatter.
+
+        for full docs, see help(logging.Formatter)
+        """
+        self._ansi_codes = {
+            'NOTSET': '',
+            'DEBUG': COLOR_BLUE,
+            'INFO': COLOR_WHITE,
+            'WARNING': COLOR_BRIGHT_YELLOW,
+            'ERROR': COLOR_RED,
+            'CRITICAL': COLOR_BRIGHT_RED,
+        }
+        super().__init__(*args, **kwargs)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Add respective color to start of record, escape code to end of record.
+
+        If unrecognized levelname, uses NOTSET formatting.
+        """
+        try:
+            ansi_start_code = self._ansi_codes[record.levelname]
+        except KeyError:
+            ansi_start_code = self._ansi_codes['NOTSET']
+
+        msg = super().format(record)
+        return ansi_start_code + msg + COLOR_RESET
+
+    def set_color(self, levelname: str, ansi_code: str) -> None:
+        """Update ansi_code assigned to levelname.
+
+        Note:
+            Capitalizes 'levelname' before storing internally.
+        """
+        if not isinstance(levelname, str):
+            raise TypeError(f"arg levelname expected 'str', not {type(levelname)}")
+        if not isinstance(ansi_code, str):
+            raise TypeError(f"arg ansi_code expected 'str', not {type(ansi_code)}")
+        self._ansi_codes[levelname.upper()] = ansi_code
+
+    def get_color(self, levelname: str) -> str:
+        """Query ansi_code assigned to levelname."""
+        if not isinstance(levelname, str):
+            raise TypeError(f"arg levelname expected 'str', not {type(levelname)}")
+        return self._ansi_codes[levelname.upper()]
+
+
+def basic_logging_config(
+        logger_names: List[str],
+        stream_log_level: int = logging.INFO,
+        log_filepath: Optional[pathlib.Path] = None,
+        format_str: str = "%(name)s.%(levelname)s: %(message)s",
+    ) -> None:
+    """For each logger name, Attach INFO streamhandler, DEBUG filehandler."""
+    stream_formatter = ColorFormatter(format_str)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(stream_log_level)
+    stream_handler.setFormatter(stream_formatter)
+
+    if log_filepath is None:
+        folder = pathlib.Path("/tmp/logs")
+        folder.mkdir(exist_ok=True)
+        temp_file = tempfile.NamedTemporaryFile(suffix='.log', delete=False, dir=folder)
+        log_filepath = temp_file.name
+
+    file_formatter = logging.Formatter(format_str)
+    file_handler = logging.FileHandler(log_filepath)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(file_formatter)
+
+    for logger_name in logger_names:
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+        logger.debug("Diagnostic logs saved to %s", log_filepath)
