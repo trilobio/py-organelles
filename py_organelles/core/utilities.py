2ee8627dff5084c56c5e9e11c7eb35aa45dbe462
Connor Novak <connor.novak@students.olin.edu>
2024-03-04 11:30:02 -0800

    Refactor: transform-tree & config-tools Modules (#575)
    * Move robot.configuration into config-tools separate package
    
    * Extracted transform_tree library from robot
    
    * Bugfix transform_tree library
    
    * Bugfix config-tools library unpack
    
    * Bugfix robot-controller tests
    
    * Fix robot imports
    
    * Work on fixing calibration imports
    
    * update environment
    
    * trilo-pyformat

---
+++ py_organelles/core/utilities.py
@@ -0,0 +1,35 @@
+"""Miscellaneous utility functions used across the Trilobio codebase."""
+import os
+import pathlib
+from difflib import get_close_matches
+
+
+def get_aceta_root() -> pathlib.Path:
+    """Check for $ACETA_ROOT environment variable, return as Path if exists."""
+    ACETA_ROOT = "ACETA_ROOT"
+    if ACETA_ROOT not in os.environ:
+        raise EnvironmentError(
+            f'Could not find environment variable "{ACETA_ROOT}".'
+            f'Add the path to the aceta repository as an environment variable called "{ACETA_ROOT}"'
+        )
+    return pathlib.Path(os.environ[ACETA_ROOT])
+
+
+def check_for_file(file_path: pathlib.Path) -> None:
+    """Raises better FileNotFoundError if file_path does not exist.
+
+    Args:
+        file_path (Path): path to file to check for existence
+
+    Raises:
+        FileNotFoundError: if file_path does not exist
+    """
+    file_path = file_path.resolve()
+    if not file_path.is_file():
+        matches = get_close_matches(
+            file_path.name, [f.name for f in file_path.parent.iterdir()]
+        )
+        closest_file = None if len(matches) == 0 else file_path.parent / matches[0]
+        raise FileNotFoundError(
+            {"missing_file": file_path, "closest_file_name": closest_file}
+        )
