9adf874fe5d86bb61c9b6a84c118278b9268f3e8
Mia Celeste <mia@trilo.bio>
2025-04-10 15:43:59 -0700

    Max made a zip file

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,16 @@
-../../run_tests.py
+"""
+This file is a basic implementation of `python -m unittest discover`. We use this
+instead of the CLI due to poetry's scripting limitation and that everything has
+to be a Python function. This is linked to all packages
+"""
+
+import sys
+import unittest
+
+
+def main():
+    loader = unittest.TestLoader()
+    runner = unittest.TextTestRunner()
+    result = runner.run(loader.discover("."))
+    if not result.wasSuccessful():
+        sys.exit(1)
