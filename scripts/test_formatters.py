0032b482c04dff529ce3a21bdfa9376bed8d0311
Connor Novak <connor@trilo.bio>
2026-06-22 19:32:32 -0700

    modify script to test copilot comment about committing from sub-directory

--- scripts/test_formatters.py
+++ scripts/test_formatters.py
@@ -5,23 +5,30 @@ import time
 
 from py_organelles.log_tools import DurationFormatter
 
-_logger_name = "__name__"
-logger = logging.getLogger(_logger_name)
-handler = logging.StreamHandler()
-formatter = DurationFormatter("%(timedelta)s: %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
-logger.setLevel(logging.INFO)
-handler.setLevel(logging.INFO)
+def main() -> None:
+    _logger_name = "__name__"
+    logger = logging.getLogger(_logger_name)
+    handler = logging.StreamHandler()
+    formatter = DurationFormatter("%(timedelta)s: %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-for i in range(1, 4):
-    time.sleep(i)
-    logger.info("Slept for %s seconds", i)
+    logger.setLevel(logging.INFO)
+    handler.setLevel(logging.INFO)
 
-formatter.update_start_time()
-logger.info("Updated start_time")
+    for i in range(1, 4):
+        time.sleep(i)
+        logger.info("Slept for %s seconds", i)
 
-for i in range(5):
-    time.sleep(i)
-    logger.info("Slept for %s seconds", i)
+    formatter.update_start_time()
+    logger.info("Updated start_time")
+
+    for i in range(5):
+        time.sleep(i)
+        logger.info("Slept for %s seconds", i)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
